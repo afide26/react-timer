f49a2b1e9cb144618058c7c9a0bdd8ea5358b1ca
Alan Fidelino <afidedev26@gmail.com>
2017-02-25 20:33:50 +1100

    Add controls for timer and tested timer components

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,10 +1,50 @@
 var React          = require('react');
-var Clock          = require('Clock')
+var Clock          = require('Clock');
+var Controls       = require('Controls')
 var Timer = React.createClass({
+  getInitialState(){
+    return({
+      count: 0,
+      timerStatus: 'stopped'
+    })
+  },
+  componentDidUpdate(prevProps, prevState){
+    if(this.state.timerStatus !== prevState.timerStatus){
+      switch(this.state.timerStatus){
+        case 'started':
+          this.handleStart();
+          break;
+        case 'stopped':
+          this.setState({count:0});
+        case 'paused':
+          clearInterval(this.timer);
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+  componentWillUnmount(){
+    clearInterval(this.timer);
+  },
+  handleStart(){
+    this.timer = setInterval(()=>{
+      this.setState({
+        count: this.state.count + 1
+      })
+    },1000);
+  },
+  handleStatusChange(newTimerStatus){
+    this.setState({
+      timerStatus:newTimerStatus
+    });
+  },
   render(){
+    var {count,timerStatus} = this.state;
     return(
       <div>
-        <Clock totalSeconds = {180}/>
+        <h1 className="page-title">Timer</h1>
+        <Clock totalSeconds = {count}/>
+        <Controls countdownStatus={timerStatus} onStatusChange = {this.handleStatusChange} />
       </div>
     )
   }
